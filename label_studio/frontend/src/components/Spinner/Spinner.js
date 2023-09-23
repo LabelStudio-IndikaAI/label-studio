709f28082bddb651ee0ea029ec7d41b46440f1f5
shreesha-j <shreesha1005@gmail.com>
2023-09-23 23:08:27 +0530

    modified:   label_studio/frontend/dist/react-app/index.js 	modified:   label_studio/frontend/dist/react-app/index.js.map 	modified:   label_studio/frontend/src/components/Spinner/Spinner.js 	new file:   label_studio/frontend/src/components/Spinner/backup.txt

--- label_studio/frontend/src/components/Spinner/Spinner.js
+++ label_studio/frontend/src/components/Spinner/Spinner.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 // import React from 'react';
 // import { cn } from '../../utils/bem';
 // import './Spinner.styl';
@@ -21,17 +20,13 @@
 //   );
 // };
 
->>>>>>> 2ff07b6ec650386ec782772175e0c03ab33e9a82
+
 // import React from 'react';
 // import { cn } from '../../utils/bem';
 // import './Spinner.styl';
 
 // export const Spinner = ({ className, style, size = 32, stopped = false }) => {
 //   const rootClass = cn('spinner');
-<<<<<<< HEAD
-
-//   const sizeWithUnit = typeof size === 'number' ? `${size}px` : size;
-=======
   
 //   const containerStyle = {
 //     width: `${size}px`,
@@ -49,7 +44,6 @@
 //     backgroundRepeat: 'no-repeat',
 //     animation: stopped ? 'none' : 'spin 1s linear infinite',
 //   };
->>>>>>> 2ff07b6ec650386ec782772175e0c03ab33e9a82
 
 //   return (
 //     <div className={rootClass.mix(className)} style={{ ...(style ?? {}), '--spinner-size': sizeWithUnit }}>
@@ -63,39 +57,6 @@
 //   );
 // };
 
-<<<<<<< HEAD
-import React from 'react';
-import { cn } from '../../utils/bem';
-import './Spinner.styl';
-import spinnerImage from './favicon.jpg';
-
-export const Spinner = ({ className, style, size = 32, stopped = false }) => {
-  const rootClass = cn('spinner');
-  
-  const containerStyle = {
-    width: `${size}px`,
-    height: `${size}px`,
-    position: 'relative',
-    ...style
-  };
-
-  const spinnerStyle = {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    backgroundImage: `url(${spinnerImage})`, // replace with your image path
-    backgroundSize: 'contain',
-    backgroundRepeat: 'no-repeat',
-    animation: stopped ? 'none' : 'spin 1s linear infinite'
-  };
-
-  return (
-    <div className={rootClass.mix(className)} style={containerStyle}>
-      <div style={spinnerStyle} />
-    </div>
-  );
-};
-=======
 
 import React from 'react';
 import { cn } from '../../utils/bem';
@@ -114,4 +75,3 @@ export const Spinner = ({ className, style, size = 32, stopped = false }) => {
     </div>
   );
 };
->>>>>>> 2ff07b6ec650386ec782772175e0c03ab33e9a82
